4c14c2b37ef7774da4201e28a005273fbb341d96
Alex <fnelclsrn123@gmail.com>
2024-01-01 19:07:34 +0900

    Update 완주하지 못한 선수.java
    예외처리 추가

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,9 +1,14 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Solution {
     
     public static String solution(String[] participant, String[] completion) {
+
+        if(Objects.isNull(participant)||Objects.isNull(completion)){
+            throw new IllegalArgumentException("입력값은 null일 수 없습니다.");
+        }
 
         Map<String, Integer> completionRunner = new HashMap<>();
         for(String runner : completion){
