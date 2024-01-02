b8622d9fa373695c3a1c279a4a7b3ea8d5efb0fa
Alex <fnelclsrn123@gmail.com>
2024-01-02 10:08:44 +0900

    변수명 리팩토링

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,23 +3,18 @@ import java.util.Set;
 
 public class Solution {
 
-
     public int solution(int[] nums) {
         int answer = 0;
+        final int MAX_UNIQUE = nums.length / 2;
 
-        Set<Integer> phoneKetMonType = new HashSet<>();
+        Set<Integer> uniquePokemons = new HashSet<>();
         
-        for(int i =0; i<nums.length; i++){
-            if(answer>=nums.length/2){
-                break;
-            }
-            if(phoneKetMonType.add(nums[i])){
+        for(int i =0; i<nums.length&& answer < MAX_UNIQUE; i++){
+            if(uniquePokemons.add(nums[i])){
                 answer++;
             } 
             
         }
-
-
         return answer;
     }
 }
