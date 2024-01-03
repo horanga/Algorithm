857b9614d986f00fec22e3f92e57e2a145b6a380
Alex <fnelclsrn123@gmail.com>
2024-01-03 12:27:45 +0900

    Update 테스트코드 추가

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -23,5 +23,98 @@ public class Solution {
                     return false;
             }
         return true;
+
+//테스트코드 
+                    @Test
+    void 정확성검사(){
+        //given
+        String[] input = {"010222", "010", "112", "222"};
+        boolean expected = false;
+        //when
+        boolean actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+}
+
+    @Test
+    void 정확성검사_오답(){
+        //given
+        String[] input = {"010222", "102", "222"};
+        boolean expected = false;
+        //when
+        boolean actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isNotEqualTo(expected);
+    }
+
+    @Test
+    void 중복번호_검사(){
+        //given
+        String[] input = {"111111111111", "11"};
+        boolean expected = false;
+        //when
+        boolean actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 번호한개(){
+        //given
+        String[] input = {"111111111111"};
+        boolean expected = true;
+        //when
+        boolean actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 빈배열(){
+        //given
+        String[] input = {};
+        boolean expected = true;
+        //when
+        boolean actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void 많은_번호(){
+        //given
+        String[] input = new String[100000];
+        for(int i = 1; i < input.length - 1; i++) {
+            // 간소화된 무작위 전화번호 생성: 8자리 숫자
+            input[i] = String.valueOf((int)(Math.random() * 100000000));
+        }
+
+        input[0]="12345678";
+        input[99999]="12345678";
+
+        boolean expected = false;
+        //when
+        boolean actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 엣지케이스(){
+        //given
+        String[] input = {"12344", "12356", "345"};
+
+        boolean expected = true;
+        //when
+        boolean actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
         }
 }
