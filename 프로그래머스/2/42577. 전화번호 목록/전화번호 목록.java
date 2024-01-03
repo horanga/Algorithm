68f0e59498e4be37d56ac4e5a2d1bc22316ada7c
Alex <fnelclsrn123@gmail.com>
2024-01-03 11:46:26 +0900

    Update 전화번호 목록.java

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,22 +1,23 @@
 public class Solution {
 
-    import java.util.HashSet;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
 
 
-    public static boolean solution(String[] phone_book) {
+        public static boolean solution(String[] phone_book) {
 
 
-        Set<String> phoneNumber = new HashSet<>();
-        for(int i =0; i< phone_book.length; i++){
-            phoneNumber.add(phone_book[i]);
+        Set<String> phoneNumberSet = new HashSet<>();
+        for(String phone: phone_book){
+            phoneNumberSet.add(phone);
         }
 
-        for(int i =0; i<phone_book.length; i++){
-            for(int j =0; j<phone_book[i].length(); j++)
-                if(phoneNumber.contains(phone_book[i].substring(0, j)))
+        //substring(startIndex, endIndex)는 startIndex부터 endIndex-1까지를 반환한다. 그래서, 시작점은 j=1로 잡아주었다. 
+        for(String phone: phone_book){
+            for(int j =1; j<phone.length(); j++)
+                if(phoneNumberSet.contains(phone.substring(0, j)))
                     return false;
             }
         return true;
