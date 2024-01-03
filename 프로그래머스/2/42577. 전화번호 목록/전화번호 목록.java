ef73b45e2a93c921c9cdb530f0ec5e36a588c3b5
Alex <fnelclsrn123@gmail.com>
2024-01-03 11:31:20 +0900

    refactoring 로직 변경

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,48 +1,24 @@
 public class Solution {
 
-    public static void main(String[] args) {
-        String[] aa ={"119", "97674223", "1195524421"};
-        solution(aa);
-    }
+    import java.util.HashSet;
+import java.util.Set;
 
-    public static boolean isContained(String longerNumber, String shortNumber){
-        for(int i=0; i<shortNumber.length(); i++){
-            if(longerNumber.charAt(i)!=shortNumber.charAt(i)){
-                return false;
-            }
-        }
-        return true;
-    }
+public class Solution {
+
 
     public static boolean solution(String[] phone_book) {
-        boolean answer = true;
 
-        String a="";
-        String b="";
 
-        for(int i =0; i<phone_book.length; i++){
-            a= phone_book[i];
-            for(int j =i+1; j<phone_book.length;j++){
-                b= phone_book[j];
-
-                if(b.indexOf(a)!=-1) {
-                    if(isContained(b, a)) {
-                        answer=false;
-                        break;
-                    }
-                } else if(a.indexOf(b)!=-1){
-                   if(isContained(a, b)) {
-                       answer = false;
-                       break;
-                   }
-
-                }
-            }
-            if(!answer){
-                break;
-            }
+        Set<String> phoneNumber = new HashSet<>();
+        for(int i =0; i< phone_book.length; i++){
+            phoneNumber.add(phone_book[i]);
         }
 
-        return answer;
-    }
+        for(int i =0; i<phone_book.length; i++){
+            for(int j =0; j<phone_book[i].length(); j++)
+                if(phoneNumber.contains(phone_book[i].substring(0, j)))
+                    return false;
+            }
+        return true;
+        }
 }
