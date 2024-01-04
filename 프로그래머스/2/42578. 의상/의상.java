d530a16bcdb9efa6e50c5f3b32345ed17f3643d7
Alex <fnelclsrn123@gmail.com>
2024-01-04 17:45:21 +0900

    refactor 테스트코드 추가

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -28,4 +28,108 @@ public int solution(String[][] clothes) {
 
         return combination;
 }
+//테스트코드
+          @Test
+    void 정상_작동(){
+
+        //given
+        String[][] input = {{"aa", "b"}, {"bb", "b"}, {"cc", "a"} , {"dd","d"}};
+        int expected = 11;
+
+        //when
+        int actual = solution(input);
+
+        //then
+      Assertions.assertThat(actual).isEqualTo(expected);
+
+
+  }
+
+    @Test
+    void 정상_작동_오답(){
+
+        //given
+        String[][] input = {{"aa", "b"}, {"bb", "b"}, {"cc", "a"} , {"dd","d"}};
+        int expected = 112;
+
+        //when
+        int actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isNotEqualTo(expected);
+
+
+    }
+
+    @Test
+    void 빈배열(){
+
+        //given
+        String[][] input = {};
+        int expected = 0;
+
+        //when
+        int actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+
+
+    }
+
+    @Test
+    void 옷_한가지(){
+
+        //given
+        String[][] input = {{"AA", "a"}, {"AB", "a"}, {"ABC", "a"}};
+        int expected = 3;
+
+        //when
+        int actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+
+
+    }
+
+    @Test
+    void 종류마다_옷한개(){
+
+        //given
+        String[][] input = {{"aa", "a"}, {"bb", "b"}, {"cc", "c"} , {"dd","d"}};
+        int expected = 7;
+
+        //when
+        int actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isNotEqualTo(expected);
+
+
+    }
+
+    @Test
+    void 대규모_데이터(){
+
+        //given
+        int numberOfTypes = 30; // 옷의 최대 종류 수
+        String[][] input = new String[numberOfTypes][2];
+
+        for (int i = 0; i < numberOfTypes; i++) {
+            input[i][0] = "Cloth" + i;
+            input[i][1] = "Type" + i;
+        }
+        int expected = (int)Math.pow(2, numberOfTypes) - 1;
+
+
+        //when
+        int actual = solution(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+
+
+    }
+        
 }
