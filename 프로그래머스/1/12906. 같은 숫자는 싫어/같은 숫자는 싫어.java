e80c49f690c80d5588e5fa68716e2ff3691afcfd
Alex <fnelclsrn123@gmail.com>
2024-01-07 16:19:03 +0900

    null 체크

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -4,12 +4,18 @@ public class Solution {
 
 public int[] solution(int[] arr) {
 
+        if (arr == null) {
+            return new int[0];
+        }
+        
         Stack<Integer> numberStack = new Stack<>();
 
         for(int number : arr){
+                if(number!=null){
             if(numberStack.isEmpty()|| numberStack.peek()!=number){
                 numberStack.push(number);
             }
+        }
         }
 
         return numberStack.stream()
