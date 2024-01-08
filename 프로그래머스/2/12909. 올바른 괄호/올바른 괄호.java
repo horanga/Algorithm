9e8b21126a74f9522e330bd2c527840f22a3267e
Alex <fnelclsrn123@gmail.com>
2024-01-08 10:37:16 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,24 +3,29 @@ import java.util.*;
 class Solution {
   static boolean solution(String s) {
 
-        if(s==null || s.length()%2!=0){//(와 )가 한 세트씩 필요하기 때문에 String의 길이는 2의 배수여야 한다.
+        if(s==null||s.length()%2!=0){
             return false;
         }
 
+
         Stack<Character> stack = new Stack<>();
         for(char ch : s.toCharArray()) {
-
-            if (ch == '(') {
+            switch (ch){
+                case '(':
                 stack.push(ch);
-            } else if (ch == ')') {
-                if (stack.isEmpty()) {
+                break;
+
+                case ')':
+                    if (stack.isEmpty()) {
                     return false;
-                } else {
+                    }
                     stack.pop();
-                }
+                    break;
+                default:
+                    return false;
             }
         }
 
-        return stack.isEmpty();//stack이 비어있지 않다면 '('나 ')'처럼 짞이 없는 괄호들이 남아있다는 뜻
+        return stack.isEmpty();
     }
 }
