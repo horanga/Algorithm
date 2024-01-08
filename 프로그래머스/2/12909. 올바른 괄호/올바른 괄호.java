880f25f1f892a9ca87514d06dd87a5da2d40b3aa
Alex <fnelclsrn123@gmail.com>
2024-01-08 10:42:07 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -28,4 +28,101 @@ class Solution {
 
         return stack.isEmpty();
     }
+
+  //테스트 코드
+
+  @Test
+    void 정상_작동(){
+        //given
+        String input ="(())()((()))";
+
+        //when
+        boolean actual = solution(input);
+
+        //then
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void 정상_작동_오답(){
+        //given
+        String input ="((((((((";
+
+        //when
+        boolean actual = solution(input);
+
+        //then
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void 빈문자열(){
+        //given
+        String input ="";
+
+        //when
+        boolean actual = solution(input);
+
+        //then
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void String_null값(){
+        //given
+        String input =null;
+
+        //when
+        boolean actual = solution(input);
+
+        //then
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void 잘못된_괄호순서(){
+        //given
+        String input =")(";
+
+        //when
+        boolean actual = solution(input);
+
+        //then
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void 다른_문자열(){
+        //given
+        String input ="(a)";
+
+        //when
+        boolean actual = solution(input);
+
+        //then
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void 긴_문자열(){
+        //when
+        String input ="(())(())()".repeat(1000);
+
+        //boolean
+        long startTime = System.nanoTime();
+        boolean actual = solution(input);
+        long endTime = System.nanoTime();
+
+        //then
+        Assertions.assertTrue(actual);
+
+        // 시간 출력 (나노초에서 밀리초로 변환)
+        System.out.println("실행 시간: " + (endTime - startTime) / 1_000_000 + " ms");
+    }
 }
