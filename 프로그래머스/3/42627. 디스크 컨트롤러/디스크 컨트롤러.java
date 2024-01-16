4f00aec3d813b46160e58731badec9e38a479943
Alex <fnelclsrn123@gmail.com>
2024-01-16 12:14:10 +0900

    Update 디스크 컨트롤러.java

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -41,5 +41,109 @@ class Solution {
 
   }
 
+//테스트코드
+            
+  @DisplayName("작업들의 대기시간 최소 평균을 계산할 수 있다.")
+  @Test
+    void testSolution(){
+      //given
+      int[][] input ={{0, 3}, {1, 9}, {2, 6}};
+      int expected =9;
+      //when
 
+      int actual = solution(input);
+
+      //then
+      assertThat(actual).isEqualTo(expected);
+  }
+
+    @DisplayName("작업들의 대기시간 최소 평균을 계산할 수 있다.(오답)")
+    @Test
+    void testSolutionWithfalseEXpected(){
+        //given
+        int[][] input ={{0, 2}, {1, 3}, {2, 6}};
+        int expected =9;
+        //when
+
+        int actual = solution(input);
+
+        //then
+        assertThat(actual).isNotEqualTo(expected);
+    }
+    @DisplayName("빈 배열을 예외처리할 수 있다.")
+    @Test
+    void testSolutionWithEmptyArray(){
+        //given
+        int[][] input ={};
+        assertThatThrownBy(()->solution(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력값이 부적절합니다.");
+
+
+  }
+
+    @DisplayName("같은 시간에 들어온 작업들의 최소 평균 대기시간을 계산할 수 있다.")
+    @Test
+    void testSolutionWithSameInt(){
+        //given
+        int[][] input ={{0, 1}, {0, 2}, {0, 3}};
+        int expected =3;
+        //when
+
+        int actual = solution(input);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("작업하나의 평균 대기 시간을 계산할 수 있다.")
+    @Test
+    void testSolutionWithOneElement(){
+        //given
+        int[][] input ={{0, 6}};
+        int expected =6;
+        //when
+
+        int actual = solution(input);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("경계값에 속하는 작업수의 최소 평균 대기 시간을 계산할 수 있다.")
+    @Test
+    void testSolutionWithBigData(){
+        //given
+
+        Random rand = new Random();
+        int[][] input = new int[500][2];
+
+
+        for(int i =0; i<500; i++){
+           input[i][0]=rand.nextInt(1001);
+           input[i][1]=1 + rand.nextInt(1000);
+        }
+
+        //when
+
+        int actual = solution(input);
+
+        //then
+        assertThat(actual).isGreaterThan(200);
+  }
+
+    @DisplayName("대기 시간이 최대치인 경우에도 최소 평균 시간을 처리할 수 있다.")
+    @Test
+    void testSolutionWith최고대기시간(){
+        //given
+
+        int[][] input = {{0, 1000}, {1,900}, {3, 1000}};
+        int expected = 1932;
+        //when
+
+        int actual = solution(input);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
