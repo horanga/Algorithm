b6bd80bc47d1f512a7bfe85008f8cb9217755399
Alex <fnelclsrn123@gmail.com>
2024-01-18 12:08:39 +0900

    Update 가장 큰 수.java

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,18 @@
 import java.util.Arrays;
 
 class Solution {
-        public static String solution(int[] numbers) {
+       public static String solution(int[] numbers) {
 
-        String[] arr = new String[numbers.length];
+        String[] numberToString = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
 
-       for (int i = 0; i < arr.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
-        }
+        Arrays.sort(numberToString, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-
-        if (arr[0].equals("0")) {
+        if (numberToString[0].equals("0")) {
             return "0";
         }
 
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            answer.append(arr[i]);
-        }
-
-        return answer.toString();
-
-
+        return String.join("", numberToString);
     }
-
 }
