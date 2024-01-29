e1221c06d79bd84efbc3bbab3dd061a9cc36ceeb
Alex <fnelclsrn123@gmail.com>
2024-01-29 21:29:18 +0900

    [level 2] Title: 구명보트, Time: 13.34 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] people, int limit) {
+        int answer =0;
+        Arrays.sort(people);
+        int index=0;
+
+        for(int i=people.length-1; i>=index; i--){
+            if(people[index]+people[i]<=limit){
+                index++;
+                answer++;
+            }
+
+            else {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
